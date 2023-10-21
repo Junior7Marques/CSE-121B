4a27bce2ac31dc9688b72a95b0d4d665fedc186e
Junior7Marques <juniorjr07@live.com>
2023-10-20 21:04:59 -0300

    final project

--- scripts/project.js
+++ scripts/project.js
@@ -1,18 +1,18 @@
-const app_id = '9fd73bb47a074d0c8b12d8cadfe3f680'; // Replace with your Open Exchange Rates app ID
-const convertButton = document.getElementById('convert');
-const resultSpan = document.getElementById('result');
+let app_id = '9fd73bb47a074d0c8b12d8cadfe3f680'; // Replace with your Open Exchange Rates app ID
+let convertButton = document.getElementById('convert');
+let resultSpan = document.getElementById('result');
 
 convertButton.addEventListener('click', () => {
-    const amount = document.getElementById('amount').value;
-    const fromCurrency = document.getElementById('fromCurrency').value;
-    const toCurrency = document.getElementById('toCurrency').value;
+    let amount = document.getElementById('amount').value;
+    let fromCurrency = document.getElementById('fromCurrency').value;
+    let toCurrency = document.getElementById('toCurrency').value;
 
     fetch(`https://openexchangerates.org/api/latest.json?app_id=${app_id}`)
         .then(response => response.json())
         .then(data => {
             if (data.rates) {
-                const exchangeRate = data.rates[toCurrency] / data.rates[fromCurrency];
-                const convertedAmount = amount * exchangeRate;
+                let exchangeRate = data.rates[toCurrency] / data.rates[fromCurrency];
+                let convertedAmount = amount * exchangeRate;
                 resultSpan.textContent = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`;
             } else {
                 resultSpan.textContent = 'Conversion failed. Please check your inputs.';
